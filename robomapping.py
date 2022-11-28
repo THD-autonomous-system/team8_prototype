92cc31f9b8425e53e798b1837a8bf4c96c16d246
Jemty Jose <107153892+Jemty@users.noreply.github.com>
2022-11-28 20:41:47 +0100

    Add files via upload

---
+++ robomapping.py
@@ -0,0 +1,33 @@
+import rospy
+from sensor_msgs.msg import LaserScan
+import matplotlib.pyplot as plt
+from math import sin, cos, pi
+
+def lidar_callback(msg):
+    global ranges, dis_forward
+    ranges=msg.ranges
+    map (ranges)
+	
+def map(ranges):
+    x=[]
+    y=[]
+    for theta in range (len(ranges)):
+        if ranges [theta] == "inf":
+            pass
+        else:
+            rad = theta *pi/180
+            x_new = -ranges [theta]*sin(rad)
+            y_new = ranges [theta]*cos(rad)
+            x.append(x_new)
+            y.append(y_new)
+    plt.clf()
+    plt.scatter(x,y)
+    plt.pause(.1)
+    plt.show
+    
+	
+rospy.init_node('map_detection')
+sub = rospy.Subscriber('/scan', LaserScan, lidar_callback)
+rospy.spin()
+
+
